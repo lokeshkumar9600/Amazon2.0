d17f681204842fc5af869a0ab219baf4b97a0ed6
lokeshkumar9600 <lokesh960077152@gmail.com>
2021-08-05 14:57:44 +0530

    cart page added

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAePPIQXfQSfv1sbQlJ8fAEWoZxaeHjTGY",
+    authDomain: "x-dbb5a.firebaseapp.com",
+    projectId: "x-dbb5a",
+    storageBucket: "x-dbb5a.appspot.com",
+    messagingSenderId: "657241173147",
+    appId: "1:657241173147:web:0aff78fc1d46cd2a9bea3e",
+    measurementId: "G-T8TBQQP8WH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
